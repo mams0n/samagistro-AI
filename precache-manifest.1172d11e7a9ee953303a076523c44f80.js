eb9fb8e186fbca25dbe5a9c8d377f256b5409086
Mamuka Mamulashvili <m.mamulashvili111@gmail.com>
2021-07-25 17:22:09 +0400

    Updates

---
+++ precache-manifest.1172d11e7a9ee953303a076523c44f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c394bcc168450ed7e3257dec4a89cd",
+    "url": "/samagistro-ai/index.html"
+  },
+  {
+    "revision": "efea2b53661a1ebf2836",
+    "url": "/samagistro-ai/static/css/main.4dcd6b70.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/samagistro-ai/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/samagistro-ai/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efea2b53661a1ebf2836",
+    "url": "/samagistro-ai/static/js/main.7ea32214.chunk.js"
+  },
+  {
+    "revision": "952efa49a212ce71f012",
+    "url": "/samagistro-ai/static/js/runtime-main.c481135a.js"
+  },
+  {
+    "revision": "3f5c69be1c57cbacd132f748f87dbad6",
+    "url": "/samagistro-ai/static/media/eurostile-lt-std-geo-mt.3f5c69be.ttf"
+  }
+]);
